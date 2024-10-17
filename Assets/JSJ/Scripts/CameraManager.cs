4d4be90ef243505372e5ec2b926801e8049b5d96
SUJI0620 <aliceji98@naver.com>
2024-10-18 00:04:34 +0900

    Feat : Camera Moving

---
+++ Assets/JSJ/Scripts/CameraManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public float followSpeed = 5f;
+
+    Vector3 offset;
+
+    void Start()
+    {
+        playerPrefab = GameObject.FindWithTag("Player");
+
+        if (playerPrefab != null)
+        {
+            offset = transform.position - playerPrefab.transform.position;
+        }
+    }
+
+    void Update()
+    {
+        CameraMoving();
+    }
+
+    public void CameraMoving()
+    {
+        Vector3 tartgetPos = playerPrefab.transform.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, tartgetPos, followSpeed * Time.deltaTime);
+    }
+}
