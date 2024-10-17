baa89fa0e99dfb7856c0705fc59a3c2252a20a51
SUJI0620 <aliceji98@naver.com>
2024-10-17 20:00:49 +0900

    Feat : Picket

---
+++ Assets/JSJ/Scripts/PicketManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PicketManager : MonoBehaviour
+{
+    public GameObject picketPrefab;
+    public GameObject player;
+
+    Vector3 picketPos;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void MakePicket()
+    {
+        picketPos = player.transform.position + player.transform.forward * 3f;
+
+        GameObject obj = Instantiate(picketPrefab, picketPos, Quaternion.identity);
+        obj.transform.LookAt(player.transform);
+    }
+}
